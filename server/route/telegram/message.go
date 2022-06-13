7ab79b043e43f5fc01c53383b72d174c3a46a55d
Omar Omar <oomar@umich.edu>
2022-06-12 19:06:59 -0700

    Add image support to message endpoint

--- server/route/telegram/message.go
+++ server/route/telegram/message.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -12,6 +13,7 @@ import (
 type TelegramMessageRequest struct {
 	ChatID  int64  `json:"chatId"`
 	Message string `json:"message"`
+	Image   string `json:"image"`
 }
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,6 +44,25 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 	msg := tgbotapi.NewMessage(tlgmMsg.ChatID, tlgmMsg.Message)
 	if _, err := BOT.Send(msg); err != nil {
 		http.Error(w, "failed to send message", http.StatusBadRequest)
+		return
+	}
+
+	// Decode, construct, and upload a given image to telegram.
+	if tlgmMsg.Image == "" {
+		return
+	}
+	img, err := base64.StdEncoding.DecodeString(tlgmMsg.Image)
+	if err != nil {
+		http.Error(w, "failed to decode image", http.StatusBadRequest)
+		return
+	}
+
+	photoFileBytes := tgbotapi.FileBytes{
+		Name:  "image",
+		Bytes: img,
+	}
+	if _, err := BOT.Send(tgbotapi.NewPhoto(tlgmMsg.ChatID, photoFileBytes)); err != nil {
+		http.Error(w, "failed to upload the image", http.StatusBadRequest)
 		return
 	}
 }
